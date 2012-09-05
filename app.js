a4600eaa412ecceac4f97d26e6013dbc7f3d90f8
gaspard <gaspard@gmail.com>
2012-09-05 16:40:10 +0200

    a sample database

--- app.js
+++ app.js
@@ -10,3 +10,30 @@ http.createServer(function(req, res) {
 	res.end(res.message+'\n');
 
 }).listen(8989);
+
+
+// debug
+var d = new Date();
+
+app.db = {
+	users:{
+		'bob': {
+			nickname: 'bob',
+			password: 'password',
+			token: 'd131dd02c5e6eec4',
+			online: true,
+			inbox: [
+				{
+					from: 'bob',
+					date: d,
+					body: 'hello to myself'
+				},
+				{
+					from: 'denise',
+					date: d,
+					body: 'hello bob !'
+				}
+			]
+		}
+	}
+};
