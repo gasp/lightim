98471d827e185457c292a21767c442b054bced55
gaspard <gaspard@gmail.com>
2012-09-05 16:56:44 +0200

    first routing attempt

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ var http = require('http'),
 	app = {},
 	utils = {};
 
-
 http.createServer(function(req, res) {
 	res.message = 'Hello ';
 	res.message += req.url.toString();
-	
+
+	app.route(req,res);
+
 	res.writeHead(res.statusCode, res.contentType);
-
-	console.log(app.db.users.bob.password);
-	
 	res.end(res.message+'\n');
-
 	
 
 }).listen(8989);
+
+
+
 
 //sample data
 // debug
@@ -42,3 +42,58 @@ app.db = {
 		}
 	}
 };
+
+
+app.route = function(req, res){
+
+	req.format = 'json';
+	res.message += ' ('+req.format+')';
+
+	switch (req.url) {
+		case '/':
+			res.statusCode = 200;
+			res.contentType = {'Content-Type': 'text/plain'};
+			
+			break;
+		case '/about/bob':
+			res.message = app.db.users['bob'].nickname + ' has ' 
+				+ app.db.users['bob'].inbox.length + ' pending messages';
+			res.statusCode = 200;
+			res.contentType = {'Content-Type': 'text/plain'};
+			break;
+		case '/tell/bob':
+			var d = new Date();
+			app.db.users['bob'].inbox.push(
+				{
+					from: 'alice',
+					date: d,
+					body: 'hello bob'
+				}
+			);
+			res.statusCode = 200;
+			res.contentType = {'Content-Type': 'text/plain'};
+
+			req.message = 'message sent to bob';
+			break;
+		case '/inbox/bob':
+
+			var message = new String();
+
+			app.db.users['bob'].inbox.forEach(function(m){
+				message +=
+					'\n\n from: '+ m.from.toString()
+					+ '\n date: '+ m.date.toString()
+					+ '\n body: '+ m.body.toString();
+			});
+
+			res.message = message;
+
+			break;
+			
+		default:
+			res.statusCode = 404;
+			res.contentType = {'Content-Type': 'text/plain'};
+			
+			break;
+	}
+}
