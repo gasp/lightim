aedc1877bdcf22d89001ae28c055299e7f1451da
gaspard <gaspard@gmail.com>
2012-09-05 16:53:21 +0200

    explore fake db's data

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 var http = require('http'),
 	app = {},
 	utils = {};
+
+
 http.createServer(function(req, res) {
 	res.message = 'Hello ';
 	res.message += req.url.toString();
 	
 	res.writeHead(res.statusCode, res.contentType);
+
+	console.log(app.db.users.bob.password);
 	
 	res.end(res.message+'\n');
 
+	
+
 }).listen(8989);
 
-
+//sample data
 // debug
 var d = new Date();
-
 app.db = {
-	users:{
+	users : {
 		'bob': {
 			nickname: 'bob',
-			password: 'password',
+			password: 'superpassword',
 			token: 'd131dd02c5e6eec4',
 			online: true,
 			inbox: [
